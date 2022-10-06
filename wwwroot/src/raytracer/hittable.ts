08a4c4ffeb82452fbcc190f90dc414ff32cad25e
joel.sutton <joel.m.sutton@gmail.com>
2022-10-06 13:02:37 +1300

    Make the concept of Hittables Sphere is a hittable Trace rays over a collection of hittables

---
+++ wwwroot/src/raytracer/hittable.ts
@@ -0,0 +1,89 @@
+import Ray from "./Ray";
+import { dot, scale, subtract, Vec3 } from "./vec3";
+
+export interface HitRecord {
+    point: Vec3;
+    normal: Vec3;
+    t: number;
+    frontFace: boolean;
+}
+
+export interface IHittable {
+    hit(ray: Ray, tMin: number, tMax: number): HitRecord | null;
+}
+
+export class HittableList implements IHittable {
+    #hittables: Array<IHittable> = [];
+
+    clear() {
+        this.#hittables = [];
+    }
+
+    add(hittable: IHittable) {
+        this.#hittables.push(hittable);
+    }
+
+    hit(ray: Ray, tMin: number, tMax: number): HitRecord | null {
+        let closestHit: HitRecord | null = null;
+
+        for (const h of this.#hittables) {
+            const hit = h.hit(ray, tMin, closestHit?.t ?? tMax);
+            closestHit = hit ?? closestHit;
+        }
+
+        return closestHit;
+    }
+}
+
+export class Sphere implements IHittable {
+    #centre: Vec3;
+    #radius: number;
+
+    constructor()
+    constructor(centre: Vec3, radius: number)
+    constructor(centre?: Vec3, radius?: number) {
+        this.#centre = centre ?? { x: 0, y: 0, z: 0};
+        this.#radius = radius ?? 0;
+    }
+
+    hit(ray: Ray, tMin: number, tMax: number): HitRecord | null {
+        // TODO: implement
+
+        // Equation for ray(O,D) intersects sphere(C,r):
+        // => t^2*(D⋅D)+2*t*(D⋅(O-C))+((O-C)⋅(O-C)-r^2)=0
+        // Solve for t to determine if ray intersects at any point
+        // => Quadratic formula: (-b +- sqrt(b^2 - 4*a*c)) / 2*a where
+        //      - a = (D⋅D)
+        //      - b = 2*(D⋅(O-C))
+        //      - c = ((O-C)⋅(O-C))−r^2
+        // => b is a multiple of 2 so divide through to simplify (h = b/2 = D⋅(O-C)): (-h +- sqrt(h^2 - a*c)) / a
+        const oc = subtract(ray.origin, this.#centre);
+        const a = dot(ray.direction, ray.direction);
+        const halfB = dot(ray.direction, oc);
+        const c = dot(oc) - this.#radius*this.#radius;
+        const discriminant = halfB*halfB - a*c;
+
+        if (discriminant < 0) return null; // Imaginary solution - no hit
+
+        const sqrtd = Math.sqrt(discriminant);
+
+        // Find the nearest root that lies in the acceptable range.
+        let t = (-halfB - sqrtd) / a; // Try smallest root
+        if (t < tMin || t > tMax) {
+            t = (-halfB + sqrtd) / a; // Try largest root
+            if (t < tMin || t > tMax) return null;
+        }
+
+        // Hit point and surface normal at hit point
+        const point = ray.at(t);
+        const [normal, frontFace] = correctNormal(ray, scale(subtract(point, this.#centre), 1 / this.#radius));
+
+        return { point, normal, t, frontFace };
+    }
+}
+
+function correctNormal(ray: Ray, normal: Vec3): [correctedNormal: Vec3, frontFace: boolean] {
+    const frontFace = dot(ray.direction, normal) < 0;
+    const correctedNormal = frontFace ? normal : scale(normal, -1);
+    return [correctedNormal, frontFace];
+}
--- wwwroot/src/raytracer/rayTracer.ts
+++ wwwroot/src/raytracer/rayTracer.ts
@@ -1,9 +1,19 @@
+import { HittableList, IHittable, Sphere } from "./hittable";
 import ImageStream from "./ImageStream";
 import Ray from "./Ray";
-import { add, dot, lerp, normalise, scale, subtract, Vec3 } from "./vec3";
+import { add, lerp, normalise, scale, subtract, Vec3 } from "./vec3";
 
 export function render(width: number, height: number, data: SharedArrayBuffer, statusUpdate: (msg: string) => void) {
     const aspectRatio = width / height;
+
+    // World
+    
+    const world = new HittableList();
+    world.add(new Sphere({ x: 0, y: 0, z: -1 }, 0.5));
+    world.add(new Sphere({ x: 0, y: -100.5, z: -1 }, 100));
+
+    // Camera
+
     const origin: Vec3 = { x: 0, y: 0, z: 0 };
     const viewportHeight = 2.0;
     const viewportWidth = aspectRatio * viewportHeight;
@@ -17,6 +27,8 @@ export function render(width: number, height: number, data: SharedArrayBuffer, s
             scale(vertical, 1/2)
         ), { x: 0, y: 0, z: focalLength }
     );
+
+    // Render
 
     const imageStream = new ImageStream(data);
 
@@ -35,50 +47,23 @@ export function render(width: number, height: number, data: SharedArrayBuffer, s
             );
             const ray = new Ray(origin, rayDir);
             
-            imageStream.putRgba(rayColour(ray), 1);
+            imageStream.putRgba(rayColour(ray, world), 1);
         }
     }
 }
 
-/**
- * @returns the location on the ray that intersects with the sphere, or -1 if no intersection occurs
- */
-function hitSphere(centre: Vec3, radius: number, ray: Ray): number {
-    // Equation for ray(O,D) intersects sphere(C,r):
-    // => t^2*(D⋅D)+2*t*(D⋅(O-C))+((O-C)⋅(O-C)-r^2)=0
-    // Solve for t to determine if ray intersects at any point
-    // => Quadratic formula: (-b +- sqrt(b^2 - 4*a*c)) / 2*a where
-    //      - a = (D⋅D)
-    //      - b = 2*(D⋅(O-C))
-    //      - c = ((O-C)⋅(O-C))−r^2
-    // => b is a multiple of 2 so divide through to simplify (h = b/2 = D⋅(O-C)): (-h +- sqrt(h^2 - a*c)) / a
-    const oc = subtract(ray.origin, centre);
-    const a = dot(ray.direction, ray.direction);
-    const halfB = dot(ray.direction, oc);
-    const c = dot(oc) - radius*radius;
-    const discriminant = halfB*halfB - a*c;
-
-    if (discriminant < 0) return -1; // Imaginary solution - no hit
-    else return (-halfB - Math.sqrt(discriminant)) / (a); // Only taking the nearest solution?
-}
-
-const spherePos = { x: 0, y: 0, z: -1 };
-const sphereRadius = 0.5;
-
 const colour1 = { x: 1.0, y: 1.0, z: 1.0 };
 const colour2 = { x: 0.5, y: 0.7, z: 1.0 };
 
-function rayColour(ray: Ray): Vec3 {
-    let t = hitSphere(spherePos, sphereRadius, ray);
+function rayColour(ray: Ray, hittable: IHittable): Vec3 {
+    const hitRecord = hittable.hit(ray, 0, Number.MAX_SAFE_INTEGER);
 
-    if (t > 0) {
-        // Use the normal vector of the sphere at the intersection point as the colour
+    if (hitRecord) {
+        // Use the normal vector of the hit object as the colour
         // but transformed so all components are in [0,1)
-        const normal = normalise(subtract(ray.at(t), spherePos));
-        return scale(add(normal, { x: 1, y: 1, z: 1 }), 0.5);
+        return scale(add(hitRecord.normal, { x: 1, y: 1, z: 1 }), 0.5);
     }
 
     const normalisedDirection = normalise(ray.direction);
-    t = 0.5 * (normalisedDirection.y + 1.0);
-    return lerp(colour1, colour2, t);
+    return lerp(colour1, colour2, 0.5 * (normalisedDirection.y + 1.0));
 }

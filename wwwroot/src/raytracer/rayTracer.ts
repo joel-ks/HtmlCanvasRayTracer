e9c16c99e59b3f0700f2c04c52b3d2db560f6913
Joel Kusasira-Sutton <joel.m.sutton@gmail.com>
2022-11-12 17:30:09 +1300

    Final scene of random spheres

--- wwwroot/src/raytracer/rayTracer.ts
+++ wwwroot/src/raytracer/rayTracer.ts
@@ -1,10 +1,10 @@
 import Camera from "./Camera";
 import { HittableList, IHittable, Sphere } from "./hittable";
 import ImageStream from "./ImageStream";
-import { DielectricMaterial, LambertianMaterial, MetalMaterial } from "./material";
+import { DielectricMaterial, IMaterial, LambertianMaterial, MetalMaterial } from "./material";
 import { degreesToRadians, rand } from "./numberUtils";
 import Ray from "./Ray";
-import { add, dot, length, lerp, multiply, normalise, scale, subtract, Vec3 } from "./vec3";
+import { add, length, lerp, multiply, normalise, randomVec3, scale, subtract, Vec3 } from "./vec3";
 
 export function render(width: number, height: number, data: SharedArrayBuffer, statusUpdate: (msg: string) => void) {
 
@@ -14,26 +14,15 @@ export function render(width: number, height: number, data: SharedArrayBuffer, s
     const maxBounces = 50;
 
     // World
-    const world = new HittableList();
-
-    const groundMaterial = new LambertianMaterial({ x: 0.8, y: 0.8, z: 0.0 });
-    const centreMaterial = new LambertianMaterial({ x: 0.1, y: 0.2, z: 0.5 });
-    const leftMaterial = new DielectricMaterial(1.5);
-    const rightMaterial = new MetalMaterial({x: 0.8, y: 0.6, z: 0.2 }, 0.0);
-
-    world.add(new Sphere({ x: 0, y: -100.5, z: -1 }, 100, groundMaterial));
-    world.add(new Sphere({ x: 0, y: 0, z: -1 }, 0.5, centreMaterial));
-    world.add(new Sphere({ x: -1, y: 0, z: -1 }, 0.5, leftMaterial));
-    world.add(new Sphere({ x: -1, y: 0, z: -1 }, -0.4, leftMaterial)); // Hollow out the left sphere
-    world.add(new Sphere({ x: 1, y: 0, z: -1 }, 0.5, rightMaterial));
+    const world = buildRandomScene();
 
     // Camera
-    const cameraPos = { x: 3, y: 3, z: 2 };
-    const cameraLookAt = { x: 0, y: 0, z: -1 };
+    const cameraPos = { x: 13, y: 2, z: 3 };
+    const cameraLookAt = { x: 0, y: 0, z: 0 };
     const cameraUp = { x: 0, y: 1, z: 0 };
     const cameraFov = degreesToRadians(20.0);
-    const cameraAperture = 2.0;
-    const cameraFocalLength = length(subtract(cameraPos, cameraLookAt));
+    const cameraAperture = 0.1;
+    const cameraFocalLength = 10.0;
     const camera = new Camera(cameraPos, cameraLookAt, cameraUp, cameraFov, aspectRatio, cameraAperture, cameraFocalLength);
 
     // Render
@@ -58,6 +47,56 @@ export function render(width: number, height: number, data: SharedArrayBuffer, s
             imageStream.putRgba(gammaCorrect(scaled), 1);
         }
     }
+}
+
+function buildRandomScene() {
+    const world = new HittableList();
+
+    const groundMaterial = new LambertianMaterial({ x: 0.5, y: 0.5, z: 0.5 });
+    world.add(new Sphere({ x: 0, y: -1000, z: 0 }, 1000, groundMaterial));
+
+    for (let a = -11; a < 11; ++a) {
+        for (let b = -11; b < 11; ++b) {
+            const centre = {
+                x: a + 0.9*rand(),
+                y: 0.2,
+                z: b + 0.9 * rand()
+            };
+
+            if (length(subtract(centre, { x: 4, y: 0.2, z: 0 })) > 0.9)
+            {
+                let material: IMaterial;
+                const chooseMat = rand();
+
+                if (chooseMat < 0.8) {
+                    // diffuse
+                    const albedo = multiply(randomVec3(), randomVec3());
+                    material = new LambertianMaterial(albedo);
+                } else if (chooseMat < 0.95) {
+                    // metal
+                    const albedo = randomVec3(0.5, 1);
+                    const fuzz = rand(0, 0.5);
+                    material = new MetalMaterial(albedo, fuzz);
+                } else {
+                    // glass
+                    material = new DielectricMaterial(1.5);
+                }
+
+                world.add(new Sphere(centre, 0.2, material));
+            }
+        }
+    }
+
+    const material1 = new DielectricMaterial(1.5);
+    world.add(new Sphere({ x: 0, y: 1, z: 0 }, 1.0, material1));
+
+    const material2 = new LambertianMaterial({ x: 0.4, y: 0.2, z: 0.1 });
+    world.add(new Sphere({ x: -4, y: 1, z: 0 }, 1.0, material2));
+
+    const material3 = new MetalMaterial({ x: 0.7, y: 0.6, z: 0.5 }, 0.0);
+    world.add(new Sphere({ x: 4, y: 1, z: 0 }, 1.0, material3));
+
+    return world;
 }
 
 function gammaCorrect(colour: Vec3) {
